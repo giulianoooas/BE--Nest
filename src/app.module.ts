09f98a24016621e3947a1d21f36a363f5748a877
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2021-10-26 23:30:08 +0300

    create typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
@@ -6,7 +8,22 @@ import { CategoryModule } from './category/category.module';
 import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [CarModule, CategoryModule, CommentModule],
+  imports: [
+    CarModule,
+    CategoryModule,
+    CommentModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
