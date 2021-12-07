aaed37bbe3f07717602b3b536a2104cf3718a6eb
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2021-12-07 23:14:24 +0200

    change user dto

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,7 +1,11 @@
 /* eslint-disable prettier/prettier */
+import { UserStatus } from "../enums/user-status.enum";
+
 export interface UserDTO{
     userId?: number;
     password: string;
     email: string;
     nickname: string;
+    logoUrl?: string;
+    userStatus?: UserStatus;
 }
